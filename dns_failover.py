564439c7435d96076e751d62825dd77b7954f115
Tara Poteat <tarapoteat@lawn-128-61-5-7.lawn.gatech.edu>
2021-03-16 11:28:20 -0400

    dns and failover

---
+++ dns_failover.py
@@ -0,0 +1,32 @@
+import dns
+import dns.resolver
+import sys
+
+websites = ['amazon.com', 'gatech.edu']
+
+for website in websites:
+	print(website)
+	# IPv4 dns addresses
+	try:
+		listOfIPv4 = dns.resolver.resolve(website, 'A')
+	except dns.resolver.NoAnswer:
+		listOfIPv4 = []
+	except dns.resolver.NXDOMAIN:
+		print('domain does not exist')
+		sys.exit(1)
+
+	# IPv6 dns addreses
+	try: 
+		listOfIPv6 = dns.resolver.resolve(website, 'AAAA')
+	except dns.resolver.NoAnswer:
+		listOfIPv6 = []
+	except dns.resolver.NXDOMAIN:
+		print('domain does not exist')
+		sys.exit(1)
+
+	for ipType in listOfIPv4, listOfIPv6:
+		for ip in ipType:
+			print('IP address: ', ip.to_text())
+
+	print('Failover (number of servers):', len(listOfIPv4) + len(listOfIPv6))
+
