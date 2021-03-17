0e3712d238c00d7c3a900b7150cad4e4ead3e440
CollinAvidano <collin.avidano@gmail.com>
2021-03-17 03:11:01 -0400

    initial code for form scrapper havent tested it yet

---
+++ form_scraper/form_scraper/settings.py
@@ -0,0 +1,42 @@
+# Scrapy settings for form_scrapper project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'form_scraper'
+
+SPIDER_MODULES = ['form_scraper.spiders']
+NEWSPIDER_MODULE = 'form_scraper.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'form_scrapper (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+# ITEM_PIPELINES = {'scrapy.pipelines.images.ImagesPipeline': 1}
+
+IMAGES_STORE = '/home/collin/documents/personal-projects/form_scraper/results'
+FILES_STORE = '/home/collin/documents/personal-projects/form_scraper/results'
+
+# IMAGES_THUMBS = {
+#     'small': (50, 50),
+#     'big': (260, 260),
+# }
