5b3d18c744ac6ad78d82a10f51ce5fc7dc8d74fd
CollinAvidano <collin.avidano@gmail.com>
2021-03-20 14:31:38 -0400

    I think im losing my mind

--- ciphersuite.py
+++ ciphersuite.py
@@ -5,14 +5,17 @@ import ssl
 
 target_url = 'google.com'
 
-ctx = ssl.create_default_context()
-socks = socket.socket()
-sock = ctx.wrap_socket(socks, server_hostname=target_url)
-sock.connect((target_url, 443))
+import subprocess
+def out(command):
+    output = subprocess.check_output(command, shell=True)
+    return output
 
-cipher = sock.cipher()
+def scan_url(url):
+    out("./cipherscan/cipherscan -j " + url + " > " + url)
 
-if cipher != None:
-	print("Cipher Name: " + str(cipher[0]))
-	print("SSL Protocol Version: " + str(cipher[1]))
-	print("Number of Secret Bits: " + str(cipher[2]))
+def main():
+    scan_url(target_url)
+
+if __name__ == "__main__":
+    main()
+
