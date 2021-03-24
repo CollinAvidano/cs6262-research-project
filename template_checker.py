41a73298fa5146fb6417a32df309ecdd5f91172d
Joshua Dierberger <jdierberger1@gmail.com>
2021-03-24 16:37:25 -0400

    yeet that template checker

---
+++ template_checker.py
@@ -0,0 +1,28 @@
+import urllib.request
+import re
+
+def check_templating(url):
+    fp = urllib.request.urlopen(url)
+    mybytes = fp.read()
+
+    mystr = mybytes.decode("utf8")
+    fp.close()
+
+    metacheck = re.search('<meta\\s+name="generator"\\s+content=.*>', mystr);
+
+    if metacheck is not None:
+        content_part = re.search('content=.*', metacheck.group(0))
+        return content_part.group(0)[8:-2]
+
+    if "<!-- This is Squarespace. -->" in mystr:
+        return "Squarespace"
+
+    if "var Shopify = Shopify" in mystr:
+        return "Shopify"
+
+    if '_W.configDomain = "www.weebly.com";' in mystr:
+        return "Weebly"
+
+    # print(mystr)
+
+print(check_templating("https://urbanasacs.com/"))
