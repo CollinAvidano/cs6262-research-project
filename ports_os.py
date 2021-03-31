aff503fdff39fae0cd1fbe54965b683060116692
CollinAvidano <collin.avidano@gmail.com>
2021-03-31 17:16:21 -0400

    I crave death

--- ports_os.py
+++ ports_os.py
@@ -4,40 +4,37 @@ import nmap
 # servers = ['205.251.242.103', '108.177.122.138', '128.61.5.7']
 # servers = ['twitter.com']
 # which ports are we trying to scan?
+
 ports = '1-500'
+
 class port_os_results:
     host_ip=''
     hostname=''
-    protocol=''
-    active_ports=''
-    location=''
+    protocols=[]
+    active_ports_tcp=[]
+    active_ports_udp=[]
 
-    def __init__(self, host_ip='', hostname='', protocol='', active_ports='', location=''):
+    def __init__(self, host_ip='', hostname='', protocol=[], active_ports_tcp=[], active_ports_udp=[]):
         self.host_ip=host_ip
         self.hostname=hostname
         self.protocol=protocol
-        self.active_ports=active_ports
-        self.location=location
+        self.active_ports_tcp=active_ports_tcp
+        self.active_ports_udp=active_ports_udp
 
+# this will give you OS version but you have to use root privledges (not sure how to make this work)
 def check_ports_os(ip):
     scanner = nmap.PortScanner()
-    #scanner.scan(server)
 
-    # this will give you OS version but you have to use root privledges (not sure how to make this work)
     scanner.scan(ip, arguments='-O')
 
-    if (len(scanner.all_hosts()) > 1):
-        print("multiple ips found when scanning single ip" ' , '.join(scanner.all_hosts()))
-        print("only returning matching ip")
-
-    for host in scanner.all_hosts():
-        if host == ip:
-
-            print('Host IP: ', host)
-            print('Host Name: ', scanner[host].hostname())
-            print('OS: ', scanner[host]['osmatch'])
-            for protocol in scanner[host].all_protocols():
-                print('Protocol : %s' % protocol)
-                activePorts = scanner[host][protocol].keys()
-                for port in activePorts:
-                    print ('port %s (%s)' % (port, scanner[host][protocol][port]['state']))
+    results = port_os_results(ip=ip, hostname=scanner[ip].hostname(), protocols=scanner[ip].all_protocols())
+    for protocol in scanner[ip].all_protocols():
+        active_ports = scanner[ip][protocol].keys()
+        for port in active_ports:
+            if protocol == 'tcp':
+                results.active_ports_tcp.append(port)
+            elif protocol == 'udp':
+                results.active_ports_udp.append(port)
+            else:
+                pass
+    return results
