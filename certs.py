b076b592c59f08360de7306c47f17f6a1fd2fcc6
Joshua Dierberger <jdierberger1@gmail.com>
2021-03-31 17:15:31 -0400

    cert works

--- certs.py
+++ certs.py
@@ -5,26 +5,36 @@
 import socket
 import ssl
 
-target_url = 'google.com'
+target_url = 'expired.badssl.com'
 
 cert = ssl.get_server_certificate((target_url, 443))
 ctx = ssl.create_default_context()
 socks = socket.socket()
 sock = ctx.wrap_socket(socks, server_hostname=target_url)
-sock.connect((target_url, 443))
-certs = sock.getpeercert()
-subject = dict(x[0] for x in certs['subject'])
-country = subject['countryName']
-issued_to = subject['commonName']
-organization = subject['organizationName']
-location = subject['stateOrProvinceName']
-issuer = dict(x[0] for x in certs['issuer'])
-issued_by = issuer['commonName']
-print(cert)
-print('Country: ' + country)
-print('Issued to: ' + issued_to)
-print('Organization: ' + organization)
-print('Province: ' + location)
-print('Issued by: ' + issued_by)
+go = True
+try:
+    sock.connect((target_url, 443))
+except ssl.SSLError as err:
+    print("Certificate is invalid (" + str(err.reason) + ")")
+    go = False
 
-# Now just have to save it
+if go:
+    certs = sock.getpeercert()
+
+    print(cert)
+
+    subject = dict(x[0] for x in certs['subject'])
+
+    to_inspect = {'countryName', 'commonName', 'organizationName', 'stateOrProvinceName'}
+    other_values = dict()
+
+    for key in to_inspect:
+        if key in subject:
+            print(str(key) + ": " + str(subject['countryName']))
+
+    issuer = dict(x[0] for x in certs['issuer'])
+    issued_by = issuer['commonName']
+
+    print('Issued by: ' + issued_by)
+
+    # Now just have to save it
