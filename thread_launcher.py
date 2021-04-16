db0e579500d2787f1c4de79a3c0fde74d184259a
Joshua Dierberger <jdierberger1@gmail.com>
2021-04-16 01:35:20 -0400

    god forbid we use a compiler right

--- thread_launcher.py
+++ thread_launcher.py
@@ -11,52 +11,6 @@ def method(url):
     time.sleep(2)
     return url + 4
 
-## Import CSV of sites
-columns = defaultdict(list) # each value in each column is appended to a list
-
-with open('rando_site.csv') as f:
-    reader = csv.DictReader(f) # read rows into a dictionary format
-
-    for row in reader: # read a row as {column1: value1, column2: value2,...}
-        for (k,v) in row.items(): # go over each column name and value
-            columns[k].append(v) # append the value into the appropriate list
-                                 # based on column name k
-
-url_list = columns['Sites'] ##List
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-)
-
-cursor = db.cursor() #object to execute SQL statements and interact with SQL server
-
-#cursor.execute("DROP DATABASE IF EXISTS website_vulnerabilities")
-#cursor.execute("CREATE DATABASE website_vulnerabilities")
-
-#cursor.execute("CREATE TABLE website_vulnerabilities.website (url VARCHAR(50) PRIMARY KEY)")
-#cursor.execute("CREATE TABLE website_vulnerabilities.sub_domain (parent_url VARCHAR(50), child_url VARCHAR(50) PRIMARY KEY, FOREIGN KEY(parent_url) REFERENCES website(url))")
-#cursor.execute("CREATE TABLE website_vulnerabilities.ip_address (url VARCHAR(50), ip_address VARCHAR(50) PRIMARY KEY, ip_version VARCHAR(50), FOREIGN KEY(url) REFERENCES website(url))")
-#cursor.execute("CREATE TABLE website_vulnerabilities.cert (url VARCHAR(50), issued_to VARCHAR(50), issued_by VARCHAR(50), organization VARCHAR(50), country VARCHAR(50), location VARCHAR(50), FOREIGN KEY(url) REFERENCES website(url))")
-#cursor.execute("CREATE TABLE website_vulnerabilities.cipher (url VARCHAR(50), cipher VARCHAR(50), PRIMARY KEY(url, cipher), FOREIGN KEY(url) REFERENCES website(url) )")
-#cursor.execute("CREATE TABLE website_vulnerabilities.port (ip_addr VARCHAR(50), port_number VARCHAR(10), protocol VARCHAR(50), PRIMARY KEY (ip_addr, port_number), FOREIGN KEY(ip_addr) REFERENCES ip_address(ip_address))")
-#cursor.execute("CREATE TABLE website_vulnerabilities.os (ip_addr VARCHAR(50), version VARCHAR(50), PRIMARY KEY (ip_addr, version), FOREIGN KEY(ip_addr) REFERENCES ip_address(ip_address))")
-#cursor.execute("CREATE TABLE website_vulnerabilities.traceroute (ip_addr VARCHAR(50), sender_ttl VARCHAR(50), receiver_source VARCHAR(50), sender_time VARCHAR(50), receiver_time VARCHAR(50), PRIMARY KEY (ip_addr, sender_ttl), FOREIGN KEY(ip_addr) REFERENCES ip_address(ip_address))")
-#cursor.execute("CREATE TABLE website_vulnerabilities.forms (url VARCHAR(50), class VARCHAR(500), type VARCHAR(50), FOREIGN KEY(url) REFERENCES website(url))")
-#cursor.execute("CREATE TABLE website_vulnerabilities.template (url VARCHAR(50) PRIMARY KEY, temp VARCHAR(500), FOREIGN KEY(url) REFERENCES website(url))")
-
-start_index = 0
-number = 10
-
-with concurrent.futures.ThreadPoolExecutor(max_workers = 8) as executor: ##Limit max number of threads to 8
-    for i in range(start_index, start_index + number):
-        future = executor.submit(main.scan_url, url_list[i], cursor, i)
-        future.add_done_callback(commit)
-
-    #for i in range(start_index, start_index + number):
-        #loop.run_in_executor(executor, main.scan_url, url_list[i], cursor, i)
-
 def commit(future):
     if (future is None or future.results() is None):
         print("None in future or results ({future} or {future.results()})")
@@ -122,3 +76,49 @@ def commit(future):
         print(f"ret[1].result() is None:{results}")
         # raise
     print("Commited {website}")
+
+## Import CSV of sites
+columns = defaultdict(list) # each value in each column is appended to a list
+
+with open('rando_site.csv') as f:
+    reader = csv.DictReader(f) # read rows into a dictionary format
+
+    for row in reader: # read a row as {column1: value1, column2: value2,...}
+        for (k,v) in row.items(): # go over each column name and value
+            columns[k].append(v) # append the value into the appropriate list
+                                 # based on column name k
+
+url_list = columns['Sites'] ##List
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+)
+
+cursor = db.cursor() #object to execute SQL statements and interact with SQL server
+
+#cursor.execute("DROP DATABASE IF EXISTS website_vulnerabilities")
+#cursor.execute("CREATE DATABASE website_vulnerabilities")
+
+#cursor.execute("CREATE TABLE website_vulnerabilities.website (url VARCHAR(50) PRIMARY KEY)")
+#cursor.execute("CREATE TABLE website_vulnerabilities.sub_domain (parent_url VARCHAR(50), child_url VARCHAR(50) PRIMARY KEY, FOREIGN KEY(parent_url) REFERENCES website(url))")
+#cursor.execute("CREATE TABLE website_vulnerabilities.ip_address (url VARCHAR(50), ip_address VARCHAR(50) PRIMARY KEY, ip_version VARCHAR(50), FOREIGN KEY(url) REFERENCES website(url))")
+#cursor.execute("CREATE TABLE website_vulnerabilities.cert (url VARCHAR(50), issued_to VARCHAR(50), issued_by VARCHAR(50), organization VARCHAR(50), country VARCHAR(50), location VARCHAR(50), FOREIGN KEY(url) REFERENCES website(url))")
+#cursor.execute("CREATE TABLE website_vulnerabilities.cipher (url VARCHAR(50), cipher VARCHAR(50), PRIMARY KEY(url, cipher), FOREIGN KEY(url) REFERENCES website(url) )")
+#cursor.execute("CREATE TABLE website_vulnerabilities.port (ip_addr VARCHAR(50), port_number VARCHAR(10), protocol VARCHAR(50), PRIMARY KEY (ip_addr, port_number), FOREIGN KEY(ip_addr) REFERENCES ip_address(ip_address))")
+#cursor.execute("CREATE TABLE website_vulnerabilities.os (ip_addr VARCHAR(50), version VARCHAR(50), PRIMARY KEY (ip_addr, version), FOREIGN KEY(ip_addr) REFERENCES ip_address(ip_address))")
+#cursor.execute("CREATE TABLE website_vulnerabilities.traceroute (ip_addr VARCHAR(50), sender_ttl VARCHAR(50), receiver_source VARCHAR(50), sender_time VARCHAR(50), receiver_time VARCHAR(50), PRIMARY KEY (ip_addr, sender_ttl), FOREIGN KEY(ip_addr) REFERENCES ip_address(ip_address))")
+#cursor.execute("CREATE TABLE website_vulnerabilities.forms (url VARCHAR(50), class VARCHAR(500), type VARCHAR(50), FOREIGN KEY(url) REFERENCES website(url))")
+#cursor.execute("CREATE TABLE website_vulnerabilities.template (url VARCHAR(50) PRIMARY KEY, temp VARCHAR(500), FOREIGN KEY(url) REFERENCES website(url))")
+
+start_index = 0
+number = 16
+
+with concurrent.futures.ThreadPoolExecutor(max_workers = 8) as executor: ##Limit max number of threads to 8
+    for i in range(start_index, start_index + number):
+        future = executor.submit(main.scan_url, url_list[i], cursor, i)
+        future.add_done_callback(commit)
+
+    #for i in range(start_index, start_index + number):
+        #loop.run_in_executor(executor, main.scan_url, url_list[i], cursor, i)
